41c235fc8d6faa3db94946eb7ea5a755a1c1a649
natanael1289 <natanaelcab1289@gmail.com>
2026-02-13 09:33:50 -0300

    resumen

--- app/page.jsx
+++ app/page.jsx
@@ -31,14 +31,14 @@ export default function Home() {
         />
       </div>
       <div className="mx-auto max-w-2xl text-center">
-        <h2 className="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">Contact sales</h2>
-        <p className="mt-2 text-lg/8 text-gray-600">Aute magna irure deserunt veniam aliqua magna enim voluptate.</p>
+        <h2 className="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">Registro</h2>
+        <p className="mt-2 text-lg/8 text-gray-600">Ingrese sus datos.</p>
       </div>
       <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div>
             <label htmlFor="first-name" className="block text-sm/6 font-semibold text-gray-900">
-              First name
+              Usuario
             </label>
             <div className="mt-2.5">
               <input
@@ -52,7 +52,7 @@ export default function Home() {
           </div>
           <div>
             <label htmlFor="last-name" className="block text-sm/6 font-semibold text-gray-900">
-              Last name
+              Contraseña
             </label>
             <div className="mt-2.5">
               <input
@@ -66,7 +66,7 @@ export default function Home() {
           </div>
           <div className="sm:col-span-2">
             <label htmlFor="company" className="block text-sm/6 font-semibold text-gray-900">
-              Company
+              Nombre
             </label>
             <div className="mt-2.5">
               <input
@@ -78,7 +78,7 @@ export default function Home() {
               />
             </div>
           </div>
-          <div className="sm:col-span-2">
+          {/* <div className="sm:col-span-2">
             <label htmlFor="email" className="block text-sm/6 font-semibold text-gray-900">
               Email
             </label>
@@ -91,8 +91,8 @@ export default function Home() {
                 className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
               />
             </div>
-          </div>
-          <div className="sm:col-span-2">
+          </div> */}
+          {/* <div className="sm:col-span-2">
             <label htmlFor="phone-number" className="block text-sm/6 font-semibold text-gray-900">
               Phone number
             </label>
@@ -138,8 +138,8 @@ export default function Home() {
                 defaultValue={''}
               />
             </div>
-          </div>
-          <div className="flex gap-x-4 sm:col-span-2">
+          </div> */}
+          {/* <div className="flex gap-x-4 sm:col-span-2">
             <div className="flex h-6 items-center">
               <div className="group relative inline-flex w-8 shrink-0 rounded-full bg-gray-200 p-px inset-ring inset-ring-gray-900/5 outline-offset-2 outline-indigo-600 transition-colors duration-200 ease-in-out has-checked:bg-indigo-600 has-focus-visible:outline-2">
                 <span className="size-4 rounded-full bg-white shadow-xs ring-1 ring-gray-900/5 transition-transform duration-200 ease-in-out group-has-checked:translate-x-3.5" />
@@ -159,14 +159,14 @@ export default function Home() {
               </a>
               .
             </label>
-          </div>
+          </div> */}
         </div>
         <div className="mt-10">
           <button
             type="submit"
             className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Let's talk
+            Registrese
           </button>
         </div>
       </form>
